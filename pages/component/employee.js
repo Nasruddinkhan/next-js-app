9c479734dd190347532bc0128e9f64ce13e64058
Nasruddinkhan <nasruddinkhan44@gmail.com>
2022-02-06 00:19:04 +0530

    add next js project

--- pages/component/employee.js
+++ pages/component/employee.js
@@ -1,7 +1,19 @@
+import Link from "next/link";
+import { Fragment } from "react";
 
 
 function EmployeeDetails(){
-    return <h1>The Employee Details Page </h1>
+    return (
+      <Fragment>
+        <h1>The Employee Pages</h1>
+        <ul>
+          <li>
+            <Link href="/component/employee/passingid">Get Employee</Link>
+          </li>
+          <li>Something Else</li>
+        </ul>
+      </Fragment>
+    );
 }
 
 export default EmployeeDetails;
